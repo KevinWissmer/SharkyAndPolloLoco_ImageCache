06992c7357290a0aa559965f3c699a0d8d833823
KevinWissmer <kevin.wissmer@mail.de>
2023-03-10 15:53:20 +0100

    image load functions added

---
+++ script/imageCache.js
@@ -0,0 +1,28 @@
+let totalImageCache = {};
+
+const loadSingleImage = path => {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.crossOrigin = 'Anonymous' // to avoid CORS if used with Canvas
+    img.src = path
+    img.onload = () => {
+      resolve(img)
+    }
+    img.onerror = e => {
+      reject(e)
+    }
+  })
+}
+
+async function loadImagesAllImages() {
+  for (let i = 0; i < imageCacheData.length; i++) {
+    const path = imageCacheData[i];
+    try {
+      const img = await loadSingleImage(path);
+      totalImageCache[path] = img;
+    } catch (e) {
+      console.log(`Problem with path: ${path}`)
+      console.log(e)
+    }
+  }
+}
